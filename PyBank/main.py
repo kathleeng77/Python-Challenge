6d6fd97f95e839dacb25cfb010b86378c1e3655c
Kathleen Graham <kathleeng77@outlook.com>
2019-06-05 11:29:14 -0500

    added greatest increase and decrease in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,10 @@ profit = []
 monthly_change = 0
 change_list = []
 avg_change = 0
+maxProfit = 0
+minProfit = 0
+indexOf_maxProfit = 0
+indexOf_minProfit = 0
 
 # read data into program
 with open(csvpath,newline='') as csvfile:
@@ -26,7 +30,9 @@ with open(csvpath,newline='') as csvfile:
 
     # following only to check if the lists were split correctly
     # print(dates)
+    # print(len(dates))
     # print(profit)
+    # print(len(profit))
 
     # print title of analysis    
     print("Financial Analysis")
@@ -40,7 +46,7 @@ with open(csvpath,newline='') as csvfile:
     total_profit = sum(profit)    
     print("Total: $" + str(total_profit))
     
-    # make list of changes
+    # loop to calc monthly_change and add to change_list
     for i in range(1, len(profit)):
         if (profit[i] and profit[i-1] > 0) or (profit[i] and profit[i-1] < 0):
             monthly_change = profit[i] - profit[i-1]
@@ -64,3 +70,26 @@ with open(csvpath,newline='') as csvfile:
     # calc and print avg_change with correct decimals
     avg_change = mean(change_list)
     print("Average Change: $" + str(format(avg_change, '.2f')))
+
+    # find greatest increase and decrease in profits
+    maxProfit = max(change_list)
+    minProfit = min(change_list)
+    
+    # check profit calc
+    # print(maxProfit)
+    # print(minProfit)
+    
+    # find index for max and min profit
+    indexOf_maxProfit = change_list.index(maxProfit)
+    indexOf_minProfit = change_list.index(minProfit)
+    
+    # check dates
+    # print(dates[indexOf_maxProfit+1])
+    # print(dates[indexOf_minProfit+1])
+    
+    # print greatest increase and decrease in profits
+    print("Greatest Increase in Profits: " + dates[indexOf_maxProfit+1] + " ($" + str(maxProfit) +")")
+    print("Greatest Decrease in Profits: " + dates[indexOf_minProfit+1] + " ($" + str(minProfit) +")")
+
+    
+
