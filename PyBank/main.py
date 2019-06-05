6f32327388913bf77559f65729b213cd095a6d2e
Kathleen Graham <kathleeng77@outlook.com>
2019-06-05 10:43:13 -0500

    added average monthly change

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,10 +5,12 @@ import csv
 # store filepath in a variable
 csvpath = os.path.join('budget_data.csv')
 
-# create empty lists
+# create lists and variables
 dates = []
 profit = []
-change = []
+monthly_change = 0
+change_list = []
+avg_change = 0
 
 # read data into program
 with open(csvpath,newline='') as csvfile:
@@ -22,17 +24,43 @@ with open(csvpath,newline='') as csvfile:
         dates.append(row[0])
         profit.append(int(row[1]))
 
-    # following code checks if the lists were split correctly
+    # following only to check if the lists were split correctly
     # print(dates)
-    # print(profit)    
+    # print(profit)
 
-    total_months = len(dates)
-    total_profit = sum(profit)
+    # print title of analysis    
     print("Financial Analysis")
     print("----------------------------")
+    
+    # calc and print total months
+    total_months = len(dates)
     print("Total Months: " + str(total_months))
+    
+    # calc and print total profit
+    total_profit = sum(profit)    
     print("Total: $" + str(total_profit))
+    
+    # make list of changes
+    for i in range(1, len(profit)):
+        if (profit[i] and profit[i-1] > 0) or (profit[i] and profit[i-1] < 0):
+            monthly_change = profit[i] - profit[i-1]
+            change_list.append(monthly_change)
+        elif profit[i] < 0 and profit[i-1] > 0:
+            monthly_change = profit[i] - profit[i-1]
+            change_list.append(monthly_change)
+        elif profit[i] > 0 and profit[i-1] < 0:
+            monthly_change = profit[i-1] - profit[i]
+            change_list.append(monthly_change)
+   
+    # following only to check correctness of list and length of list
+    # print(change_list)
+    # print(len(change_list))
 
-    # def mean(numbers):
-    # avg = sum(numbers) / len(numbers)
-    # return avg
+    # define function to get average
+    def mean(numbers):
+        avg = sum(numbers) / len(numbers)
+        return avg
+    
+    # calc and print avg_change with correct decimals
+    avg_change = mean(change_list)
+    print("Average Change: $" + str(format(avg_change, '.2f')))
