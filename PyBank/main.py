a5f363f06a832dbedce3c3a53792b65f838471d2
Kathleen Graham <kathleeng77@outlook.com>
2019-06-06 17:58:42 -0500

    improved readability/comments/txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -29,74 +29,49 @@ with open(csvpath,newline='') as csvfile:
         profit.append(int(row[1]))
 
     # following only to check if the lists were split correctly
-    # print(dates)
-    # print(len(dates))
-    # print(profit)
-    # print(len(profit))
+    # print(dates, len(dates), profit, len(profit))
     
-    # loop to calc monthly_change and add to change_list
+    # loop to calc monthly_change and append all to change_list 
     for i in range(1, len(profit)):
-        if (profit[i] and profit[i-1] > 0) or (profit[i] and profit[i-1] < 0):
+        # you must subtract 2nd number from 1st number in these 3 situations:
+        # if numbers are both positive, if numbers are both negative,
+        # if 1st number is positive and 2nd number is negative
+        if (profit[i] and profit[i-1] > 0) or (profit[i] and profit[i-1] < 0) or (profit[i] < 0 and profit[i-1] > 0):
             monthly_change = profit[i] - profit[i-1]
             change_list.append(monthly_change)
-        elif profit[i] < 0 and profit[i-1] > 0:
-            monthly_change = profit[i] - profit[i-1]
-            change_list.append(monthly_change)
+        # you must subtract 1st number from 2nd number: if 2nd number is positive and 1st number is negative 
         elif profit[i] > 0 and profit[i-1] < 0:
             monthly_change = profit[i-1] - profit[i]
             change_list.append(monthly_change)
    
     # following only to check correctness of list and length of list
-    # print(change_list)
-    # print(len(change_list))
+    # print(change_list, len(change_list))
 
-    # define function to get average
-    def mean(numbers):
-        avg = sum(numbers) / len(numbers)
-        return avg
-
-    # find greatest increase and decrease in profits
+    # find greatest increase and decrease in profits and their indices
     maxProfit = max(change_list)
     minProfit = min(change_list)
-    
-    # check profit calc
-    # print(maxProfit)
-    # print(minProfit)
-    
-    # find index for max and min profit
     indexOf_maxProfit = change_list.index(maxProfit)
     indexOf_minProfit = change_list.index(minProfit)
     
-    # check dates
-    # print(dates[indexOf_maxProfit+1])
-    # print(dates[indexOf_minProfit+1])
-    
-    # print title of analysis    
-    print("Financial Analysis")
-    print("----------------------------")
-    
-    # calc and print total months
+    # calc total months and total profit
     total_months = len(dates)
-    print("Total Months: " + str(total_months))
+    total_profit = sum(profit)
     
-    # calc and print total profit
-    total_profit = sum(profit)    
-    print("Total: $" + str(total_profit))
-    
-    # calc and print avg_change with correct decimals
+    # define average function and calc avg_change
+    def mean(numbers):
+        avg = sum(numbers) / len(numbers)
+        return avg
     avg_change = mean(change_list)
-    print("Average Change: $" + str(format(avg_change, '.2f')))
 
-    # print greatest increase and decrease in profits
-    print("Greatest Increase in Profits: " + dates[indexOf_maxProfit+1] + " ($" + str(maxProfit) +")")
-    print("Greatest Decrease in Profits: " + dates[indexOf_minProfit+1] + " ($" + str(minProfit) +")")
+    # print title of analysis, total months, total profit, avg_change with correct decimals, greatest increase and decrease
+    print(f"Financial Analysis\n----------------------------\nTotal Months: {str(total_months)}")
+    print(f"Total: ${str(total_profit)}\nAverage Change: ${str(format(avg_change, '.2f'))}")
+    print(f"Greatest Increase in Profits: {dates[indexOf_maxProfit+1]} (${str(maxProfit)})")
+    print(f"Greatest Decrease in Profits: {dates[indexOf_minProfit+1]} (${str(minProfit)})")
     
     # output all print statements to text file
-    with open('PyBank.txt', 'w') as f:
-        print("Financial Analysis", file=f)
-        print("----------------------------", file=f)
-        print("Total Months: " + str(total_months), file=f)
-        print("Total: $" + str(total_profit), file=f)
-        print("Average Change: $" + str(format(avg_change, '.2f')), file=f)
-        print("Greatest Increase in Profits: " + dates[indexOf_maxProfit+1] + " ($" + str(maxProfit) +")", file=f)
-        print("Greatest Decrease in Profits: " + dates[indexOf_minProfit+1] + " ($" + str(minProfit) +")", file=f)
+    with open('Finance.txt', 'w') as f:
+        print(f"Financial Analysis\n----------------------------\nTotal Months: {str(total_months)}", file=f)
+        print(f"Total: ${str(total_profit)}\nAverage Change: ${str(format(avg_change, '.2f'))}", file=f)
+        print(f"Greatest Increase in Profits: {dates[indexOf_maxProfit+1]} (${str(maxProfit)})", file=f)
+        print(f"Greatest Decrease in Profits: {dates[indexOf_minProfit+1]} (${str(minProfit)})", file=f)
